ebd2ec0ea91b75433315bc0188a24e58cef46dcb
Tristan Guigue <tristan@tyba.com>
2015-05-07 20:56:41 +0200

    Serve static files in production

--- houseprice/wsgi.py
+++ houseprice/wsgi.py
@@ -16,3 +16,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "houseprice.settings")
 
 application = Cling(get_wsgi_application())
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
